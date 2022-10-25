79b04fd9032e298a8bc9fe1ac8475b87732a549e
NataliaBorodina <borodinn@list.ru>
2022-10-25 14:13:27 +0600

    edited homework-20

--- homework20.js
+++ homework20.js
@@ -25,32 +25,20 @@ console.log('Сумма введеных чисел: ' + calcSum);
 // Упражнение 3
 
 function getNameOfMonth(month) {
-    if (month === 0) {
-        return ('Январь');
-    } else if (month === 1) {
-        return ('Февраль');
-    } else if (month === 2) {
-        return ('Март');
-    } else if (month === 3) {
-        return ('Апрель');
-    } else if (month === 4) {
-        return ('Май');
-    } else if (month === 5) {
-        return ('Июнь');
-    } else if (month === 6) {
-        return ('Июль');
-    } else if (month === 7) {
-        return ('Август');
-    } else if (month === 8) {
-        return ('Сентябрь');
-    } else if (month === 9) {
-        return ('Октябрь');
-    } else if (month === 10) {
-        return ('Ноябрь');
-    } else if (month === 11) {
-        return ('Декабрь');
-    } else {
-        alert ("Введите корректное значение месяца")
+    if (month === 0) return 'Январь';
+    if (month === 1) return ('Февраль');
+    if (month === 2) return ('Март');
+    if (month === 3) return ('Апрель');
+    if (month === 4) return ('Май');
+    if (month === 5) return ('Июнь');
+    if (month === 6) return ('Июль');
+    if (month === 7) return ('Август');
+    if (month === 8) return ('Сентябрь');
+    if (month === 9) return ('Октябрь');
+    if (month === 10) return ('Ноябрь');
+    if (month === 11) return ('Декабрь');
+    else {
+        alert ("Введите корректное значение месяца");
     }
 }
 let value = +prompt("Введите порядковый номер месяца от 0 до 11");
