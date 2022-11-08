e954b9abeeef7ccf847864f3d9db164d6f35f69f
NataliaBorodina <borodinn@list.ru>
2022-11-08 19:12:28 +0600

    hw-23, correction

--- homework23.js
+++ homework23.js
@@ -2,24 +2,34 @@
 
 // Упражнение 1 (вариант 1)
 
-let count = prompt('Введите число');
+// let count = prompt('Введите число');
 
-let intervalId = setInterval(() => {
+// if (isNaN(count)) {
+//     console.log('Ошибка! Вы ввели не число');
+// } else {
 
-    if (isNaN(count)) {
-        console.log('Ошибка! Вы ввели не число');
-        clearInterval(intervalId);
-    } else {
-        count = count - 1;
-        if (count >= 1) {
-            console.log('Осталось ' + count);
-        }
-        if (count === 0) {
-            console.log('Время вышло!');
-            clearInterval(intervalId);
-        }
-    }
-}, 1000);
+//     let intervalId = setInterval(() => {
+//         console.log('intervalId', intervalId)
+
+//         count = count - 1;
+//         if (count >= 1) {
+//             console.log('Осталось ' + count);
+//         }
+//         if (count === 0) {
+//             console.log('Время вышло!');
+//             clearInterval(intervalId);
+//         }
+//     }, 1000);
+// };
+
+// let count = 10; // пример из лекции
+// let intervalId = setInterval(() => {
+//     console.log('intervalId', intervalId)
+//     count = count - 1;
+//     console.log(count);
+//     if (count===5) {
+//         clearInterval(intervalId);}
+//     }, 1000);
 
 
 // Упражнение 1 (вариант 2)
@@ -29,19 +39,21 @@ let count = prompt('Введите число');
 
 let promise = new Promise((resolve, reject) => {
 
-    let intervalId = setInterval(() => {
-        count = count - 1;
-        if (count >= 1) {
-            console.log('Осталось ' + count);
-        }
-        if (count === 0) {
-            clearInterval(intervalId);
-            resolve('Время вышло!');
-        }
-    }, 1000);
 
     if (isNaN(count)) {
         reject((new Error('Ошибка! Вы ввели не число')));
+    } else {
+
+        let intervalId = setInterval(() => {
+            count = count - 1;
+            if (count >= 1) {
+                console.log('Осталось ' + count);
+            }
+            if (count === 0) {
+                clearInterval(intervalId);
+                resolve('Время вышло!');
+            }
+        }, 1000);
     };
 });
 
@@ -55,22 +67,22 @@ promise.then(
     }
 );
 
-// Упражнение 2
+// // Упражнение 2
 
-console.time('Скорость выполнения');
+// console.time('Скорость выполнения');
 
-let promise = fetch("https://reqres.in/api/users");
+// let promise = fetch("https://reqres.in/api/users");
 
-promise
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        let users = response.data;
-        console.log('Получили пользователей: ' + (users.length));
-        users.forEach(function (user) {
-            console.log(`- ${user.first_name} ${user.last_name} (${user.email})`)
-        })
-    });
+// promise
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(response) {
+//         let users = response.data;
+//         console.log('Получили пользователей: ' + (users.length));
+//         users.forEach(function (user) {
+//             console.log(`- ${user.first_name} ${user.last_name} (${user.email})`)
+//         })
+//     });
 
-console.timeEnd('Скорость выполнения');
+// console.timeEnd('Скорость выполнения');
