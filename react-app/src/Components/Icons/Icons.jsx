06803bc55295ec1de004d1774aecbd6a0f4fc61e
NataliaBorodina <borodinn@list.ru>
2022-12-01 13:21:41 +0600

    hw-32: Styled Components in component Header

---
+++ react-app/src/Components/Icons/Icons.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import FavoritesCount from '../FavoritesCount/FavoritesCount';
+import BusketCount from '../BusketCount/BusketCount';
+
+import './Icons.css';
+
+
+function Icons() {
+
+    return (
+        <div className="logo__icons">
+            <FavoritesCount />
+            <BusketCount />
+        </div>
+    );
+};
+
+export default Icons;
